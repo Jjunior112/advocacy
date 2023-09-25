2ed20e4a0ce7415173a7724dc925604c7fac5734
Jjunior112 <juarezgcjr@gmail.com>
2023-09-25 20:47:18 -0300

    seção de services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,8 +1,24 @@
-
+import { GoLaw } from 'react-icons/go'
+import { TbReportMoney } from 'react-icons/tb'
+import { BiBookOpen } from 'react-icons/bi'
+import { MdChildFriendly } from 'react-icons/md'
+import { LuLandmark } from 'react-icons/lu'
+import { FaHandcuffs } from 'react-icons/fa6'
 
 const Services = () => {
   return (
-    <div className="services" id="services">Services</div>
+    <div className="services" id="services">
+      <h1>Nossos Serviços</h1>
+      <div className="serviceIcons">
+        <button><a href="#"><GoLaw /></a></button>
+        <button><a href="#"><TbReportMoney /></a></button>
+        <button><a href="#"><BiBookOpen /></a></button>
+        <button><a href="#"><MdChildFriendly /></a></button>
+        <button><a href="#"><LuLandmark /></a></button>
+        <button><a href="#"><FaHandcuffs /></a></button>
+      </div>
+
+    </div>
   )
 }
 
