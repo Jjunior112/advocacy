a410075b28d2e629795289450c2fb577784193f2
Jjunior112 <juarezgcjr@gmail.com>
2023-09-25 19:29:02 -0300

    seção about us

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,16 @@
 
 const About = () => {
   return (
-    <div className="about" id="about">About</div>
+    <div className="about" id="about">
+      <section>
+        <h1>Quem Somos</h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit harum officiis ipsa nisi consequuntur, voluptatem id quod veritatis nam repellat, placeat consectetur excepturi unde exercitationem recusandae facere facilis. Illo, repellendus fuga, corporis laudantium maxime magnam aspernatur non distinctio sequi deserunt quos sint facere est repellat ex corrupti! Maxime, amet blanditiis?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dolores fugit commodi iste mollitia ratione voluptatibus, cum sint a nostrum natus consequatur aspernatur, cumque delectus pariatur nesciunt molestias minima necessitatibus quam soluta dolorum impedit corrupti possimus. Recusandae accusantium assumenda excepturi numquam cupiditate, sapiente perferendis. Inventore itaque nisi, sint nobis porro autem officiis totam, quia quos quasi quod, deserunt ipsum sequi!</p>
+       
+      </section>
+      <img src="whoWeAre.jpg" alt="Duas pessoas batendo as mãos em comemoração" />
+    </div>
   )
 }
 
