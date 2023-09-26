8a52fd7b6625a9047e20d57011e96b6bd9386cf8
Jjunior112 <juarezgcjr@gmail.com>
2023-09-26 19:54:37 -0300

    seção de contato

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,15 @@
 
 const Contact = () => {
   return (
-    <div className="contact" id="contact">Contact</div>
+    <div className="contact" id="contact">
+      <h1>Contato</h1>
+      <form >
+        <input type="text" placeholder="Digite seu nome" required />
+        <input type="email" placeholder="Digite seu e-mail" required />
+        <input type="tel" placeholder="Digite seu telefone" required />
+        <textarea name="" id="" cols="30" rows="10" placeholder="Digite sua mensagem" required></textarea>
+      </form>
+    </div>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,17 @@
+import { GiJusticeStar } from 'react-icons/gi'
 
 const NavBar = () => {
   return (
     <nav>
-        <img src="teste.png" alt="" />
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">Quem somos</a></li>
-          <li><a href="#services">Serviços</a></li>
-          <li><a href="#clients">Depoimentos</a></li>
-          <li><a href="#contact">Contato</a></li>
-          <li><button>Agende um Atendimento</button></li>
-        </ul>
+      <GiJusticeStar />
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">Quem somos</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#clients">Depoimentos</a></li>
+        <li><a href="#contact">Contato</a></li>
+        <li><button><a href="#contact">Agende um Atendimento</a></button></li>
+      </ul>
     </nav>
   )
 }
