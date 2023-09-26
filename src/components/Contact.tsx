0d6e761f5d3f1b9f574dd2f84bb610684fdbee38
Jjunior112 <juarezgcjr@gmail.com>
2023-09-26 20:49:11 -0300

    nova seção de contato

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,17 @@
+import { IoLogoWhatsapp } from 'react-icons/io'
+import { BiLogoGmail } from 'react-icons/bi'
 
 const Contact = () => {
+
   return (
     <div className="contact" id="contact">
       <h1>Contato</h1>
-      <form >
-        <input type="text" placeholder="Digite seu nome" required />
-        <input type="email" placeholder="Digite seu e-mail" required />
-        <input type="tel" placeholder="Digite seu telefone" required />
-        <textarea name="" id="" cols="30" rows="10" placeholder="Digite sua mensagem" required></textarea>
-      </form>
+      <section>
+        <a href="https://api.whatsapp.com/send?phone=31982208900" target='__blank'><IoLogoWhatsapp /></a>
+        <a href="mailto:juarezgcjr@gmail.com" target='__blank'><BiLogoGmail /></a>
+
+
+      </section>
     </div>
   )
 }
