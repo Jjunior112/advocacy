f06b1f91fdc1b0e304eee5fe027dc0a0c8d9bbb7
Jjunior112 <juarezgcjr@gmail.com>
2023-09-27 20:22:43 -0300

    alteraçao das imagens do slider da seção de clientes

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -5,10 +5,24 @@ import 'react-slideshow-image/dist/styles.css'
 
 const Clients = () => {
 
-    const slideImages = ['services.jpg', 'contact.jpg', 'home.jpg'
-
+    const slideImages = [
+        {
+            url: 'client1.jpg',
+            caption: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eligendi quod aperiam, omnis deleniti ullam dolor optio adipisci expedita odit aliquid accusamus at vero necessitatibus vel reiciendis, repudiandae tempore ducimus?"'
+        },
+        {
+            url: 'client2.jpg',
+            caption: '"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis necessitatibus quidem, corrupti a magni animi repudiandae ex laboriosam id. Odit enim corporis voluptatem consequatur quidem provident mollitia earum, eius, ad quaerat hic. Nesciunt, debitis."'
+        },
+        {
+            url: 'client3.jpg',
+            caption: '"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ea eos voluptate sit exercitationem quibusdam!"'
+        },
+        {
+            url: 'client4.jpg',
+            caption: '"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ea eos voluptate sit exercitationem quibusdam!"'
+        },
     ];
-
     return (
         <div className="clients" id="clients">
 
@@ -18,7 +32,8 @@ const Clients = () => {
                 <Slide>
                     {slideImages.map((slideImage, index) => (
                         <div key={index}>
-                            <div className='slider' style={{ 'backgroundImage': `url(${slideImage})` }}>
+                            <div className='slider' style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+                                <span >{slideImage.caption}  </span>
 
                             </div>
                         </div>
