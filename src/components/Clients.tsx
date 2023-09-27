c35d7b0c3a112b7d2f0eef0225119253f363c385
Jjunior112 <juarezgcjr@gmail.com>
2023-09-27 20:00:38 -0300

    navbar fixa e seção de clientes

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,7 +1,31 @@
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+
 
 const Clients = () => {
+
+    const slideImages = ['services.jpg', 'contact.jpg', 'home.jpg'
+
+    ];
+
     return (
-        <div className="clients" id="clients">Clients</div>
+        <div className="clients" id="clients">
+
+            <h1>Nossos Clientes</h1>
+
+            <div className="slide-container">
+                <Slide>
+                    {slideImages.map((slideImage, index) => (
+                        <div key={index}>
+                            <div className='slider' style={{ 'backgroundImage': `url(${slideImage})` }}>
+
+                            </div>
+                        </div>
+                    ))}
+                </Slide>
+            </div>
+        </div>
     )
 }
 
