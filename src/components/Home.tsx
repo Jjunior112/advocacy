df85db9c724861a30c7ac8080aae3a19bbef2f2b
jjunior112 <juarezgjcr@gmail.com>
2024-10-06 12:41:57 -0300

    ajuste do titulo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,10 +2,12 @@
 const Home = () => {
     return (
         <div className="home" id="home">
+
             <div>
                 <h1>Advogados associados</h1>
                 <button>Saiba mais</button>
             </div>
+
         </div>
     )
 }
